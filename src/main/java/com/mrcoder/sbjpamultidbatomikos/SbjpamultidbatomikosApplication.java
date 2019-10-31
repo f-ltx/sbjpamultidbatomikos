a34e1064644cf5d506c378f7171f27786c1ac3bf
litianxiang <litianxiang@topnet.net.cn>
2019-10-31 11:22:53 +0800

    springboot2+jpa+jta+atomikos分布式事务

---
+++ src/main/java/com/mrcoder/sbjpamultidbatomikos/SbjpamultidbatomikosApplication.java
@@ -0,0 +1,13 @@
+package com.mrcoder.sbjpamultidbatomikos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbjpamultidbatomikosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbjpamultidbatomikosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrcoder/sbjpamultidbatomikos/SbjpamultidbatomikosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrcoder.sbjpamultidbatomikos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbjpamultidbatomikosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
